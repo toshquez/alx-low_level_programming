4e94cb8566a01b2d36775d15cb3f773c9a46f613
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-09 15:40:47 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char a = 'a';
+
+
+
+			while (a <= 'z')
+
+					{
+
+								putchar(a);
+
+										a++;
+
+											}
+
+
+
+				a = 'A';
+
+
+
+					while (a <= 'Z')
+
+							{
+
+										putchar(a);
+
+												a++;
+
+													}
+
+
+
+						putchar('\n');
+
+							return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
