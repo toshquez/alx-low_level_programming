8b99f2a4f155255a69d3b6f57ee6cf62fd2345c1
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-09 02:05:21 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
  #include <stdio.h>
  /**
-  * main - Entry point
+  * main - print out stdout
   * Return: Always 0
-  *\
+  */
  int main(void)
 {
    	printf("with proper grammar, but the outcome is a piece of art, \n");
