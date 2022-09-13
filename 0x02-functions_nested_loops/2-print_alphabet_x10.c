6211f2257622e57c00651b5f134395166c294c6c
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-13 10:17:27 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	char h;
+
+	for (h = 0; h <= 9; h++)
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
+}	
