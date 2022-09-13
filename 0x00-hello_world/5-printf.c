328a1af47285b86939683a153b7fac0a792b3c4f
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-13 08:37:06 -0700

    Task 0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
   * main - print out stdout
   * Return: 0
   */
-int main(void)
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
