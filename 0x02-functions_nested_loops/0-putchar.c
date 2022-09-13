6a76140276ecd6b8ba7f46200fac028a177de838
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-13 07:57:56 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,14 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always returns 0
- *
  */
 int main(void)
 {
-	char str[100] ;
-        int ch, i, n = 0 ;
-
-        while ((ch = getchar () ) != EOF && n < 1000)
-	str [ n ++] = ch;
-
-        for (i =0; i < n; ++i)
-	putchar (str [i]);
-        
-       putchar('\n');	
+	-putchar('H');
+	-putchar('e');
+	-putchar('l');
+	-putchar('o');
 	return (0);
 }
