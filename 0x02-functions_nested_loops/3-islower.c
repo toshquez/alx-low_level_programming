c9e428e3447fe568f5653421dbab33ee5da38e59
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-13 10:49:22 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.c'
+/**
+* main - check the code
+*
+* Return: 1 if true, 0 if false
+*/
+void int _islower(void)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	return (0);
+}
