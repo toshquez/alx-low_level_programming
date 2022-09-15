03fd68227dcb8f6bab5e6be5096c8d3339d823ea
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-15 16:36:21 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,23 @@
-iiiclude "main.h"
+#include "main.h"
 
 /**
- *more_numbers - entry point
- *Description: Prints the numbers with _putchar
- *Return: void
+ * more_numbers - print 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
 
 void more_numbers(void)
 {
-	int number, tens, units, row;
+	int i, j;
 
-	for (row = 1; row <= 10; row++)
+	for (i = 0; i < 10; i++)
 	{
-        	for (number = 0; number <= 14; number++)
-		{
-                	tens = number / 10;
-			units = number % 10;
-			if (number > 9)
-                                _putchar(tens + '0');
-
-			_putchar(units + '0');
-		}
-		_putchar('\n');
+	for (j = 0; j < 15; j++)
 	}
+	if (j >= 10)
+	_putchar(j / 10 + '0');
+	_putchar(j % 10 + '0');
+	{
+	_putchar("\n");
+	}
+
 }
