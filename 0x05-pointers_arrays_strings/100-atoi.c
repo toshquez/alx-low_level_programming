d1e98aff51aea76ee802e7e0204205f4174e6d7a
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-19 14:27:18 -0700

    Task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,23 +2,35 @@
 
 /**
  * _atoi - converts a string to an int
- * @s: the string to be changed
- * Return: the converted int
+ * @s: the pointer to convert
+ * Return: A integer
  */
 
 int _atoi(char *s)
 {
-	int sign = 1;
-	insigned int num = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	do {
-		if (*s == '-')
-			sign *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
-		else if (num > 0)
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
 			break;
-		while (*s++);
+		}
+		c++;
 	}
-	return (num * sign);
+	ni *= min;
+	return (ni);
 }
