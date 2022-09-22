b67e7c491da8a56c0748dc386bf100477c610f15
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-22 04:10:07 -0700

    Task 7

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,23 +11,5 @@ char *cap_string(char *s)
 	int i, j, separatorFound;
 
 	separatorFound = 1;
-	for (i = 0; s[i]; ++i)
-	{
-		if (separatorFound && s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = s[i] - 32;
-		}
-		separatorFound = 0;
-		for (j = 0; j < 12; j++)
-		{
-			if (s[i] == '\t' || s[i] == '\n' || s[i] == ','
-			    || s[i] == '\"' || s[i] == '.' || s[i] == '!' ||
-			    s[i] == '{' || s[i] == '}' || s[i] == '(' ||
-			    s[i] == ')' || s[i] == ' ' || s[i] == '?')
-			{
-				separatorFound = 1;
-			}
-	        }
-	}
-	return (s);
+	for (i = 0)
 }
