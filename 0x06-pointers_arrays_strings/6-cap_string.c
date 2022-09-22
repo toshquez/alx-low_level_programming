893ef41b8600b45fa9977dc344ada285742aa94a
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-22 05:05:48 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,24 @@
 
 char *cap_string(char *s)
 {
-	int i, j, separatorFound;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	separatorFound = 1;
-	for (i = 0)
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A'
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+			}       }
+		}
+		i++;
+	}
+	return (s);
 }
