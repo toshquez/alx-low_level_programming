b809cdbe7ce14f3624637cd464de1b617baee83f
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-22 03:26:18 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,20 +2,24 @@
 
 /**
  * string_toupper - change lowercase to uppercase
- * @s: string
- * Return:char
+ * @s: checked
+ * Return: s
  */
 
 char *string_toupper(char *s)
 {
-	int i;
+	int c = 0;
 
-	i = 0;
-	while (*(s + i))
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-			*(s + i) -= 'a' - 'A';
-		i++
+		if (s[c] >= 65 && s[c] <= 90)
+		{
+			s[c] = s[c] + 32;
+		}
+		if (s[c] >= 97 && s[c] <= 122)
+		{
+			s[c] = s[c] - 32;
+		}
 	}
         return (s);
 }
