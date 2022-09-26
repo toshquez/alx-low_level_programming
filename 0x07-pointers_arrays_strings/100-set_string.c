ccf4c8a1d01489cc3409b5e4c9f3d96c454878d3
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-26 13:19:45 -0700

    Task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of the pointer to a char
+ * @s: source address
+ * @to: targest\\t address
+ * Return: no return value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
