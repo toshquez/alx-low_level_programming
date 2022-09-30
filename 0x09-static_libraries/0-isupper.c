718768a3c84fffa5d5f3a2500633e119a305c497
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-09-30 10:21:41 -0700

    0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function to print uppercase
+ * @c: integer assigned
+ * Return: 1 if true, else 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
