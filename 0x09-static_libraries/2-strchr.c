471620f665f1dcb6666cd0891cdbc75a883b0641
Akanbi Murthador Omotosho <111050454+toshquez@users.noreply.github.com>
2022-09-30 20:10:24 +0100

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -6,16 +6,3 @@
  * @s: in the string
  * Return: a pointer to the first occurrence of the character
  */
-char *_strchr(char *s, char c)
-{
-	while (*s)
-	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
-	}
-	if (c == '\0')
-		return (s);
-	return (NULL);
-}
