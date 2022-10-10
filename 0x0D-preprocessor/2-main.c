c4d750d5b8eb1e1352f6160678daa955059c0ade
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-10-10 06:14:59 -0700

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-main.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdio.h>
+
+/**
+ * main prints the nmae of the file of the program
+ * 	was compiled from, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", --FILE--);
+
+	return (0);
+}
