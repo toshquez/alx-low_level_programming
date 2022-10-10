8b0df5e7acc64f28763107da581195734592eaef
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-10-10 06:42:24 -0700

    Task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a function
+  -like macro SUM(x, y)
+  *	that computes the sum of the number
+ s x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
