8cf72a8ddbd17d60aee6d2fd44c453c6f2e77cdd
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-10-12 09:30:56 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: Name to be printed.
+ * @f: pointer to a function that prints a name.
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
