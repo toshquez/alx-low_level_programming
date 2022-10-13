fe8e66f9cb124ed1a5464e3015ea334efd54bf2c
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-10-12 18:07:19 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 /**
  * print_name - a function that prints a name.
  * @name: the name given
- * @f: function of name
+ * @f: function pointer
+ * Return:void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		return;
+	if (name == NULL || f == NULL)
 	f(name);
 }
