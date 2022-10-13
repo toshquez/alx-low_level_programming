d3b68a7601a4ffddcd1c84d3dab9e4ec6dc24ef7
toshquez <akanbimurthadoromotosho1347@gmail.com>
2022-10-12 18:15:09 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,7 @@
  * @size: size of the array
  * @action: pointer to the function we need to use
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array == NULL || action == NULL)
 		return;
